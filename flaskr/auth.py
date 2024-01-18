1cd836f018390ca1a4db4ce70766290d7282e788
dazeddd <joonsoolee14@gmail.com>
2024-01-18 13:02:23 +0900

    add auth template

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -34,7 +34,7 @@ def register():
                 return redirect(url_for("auth.login"))
         flash(error)
 
-    return render_template('')
+    return render_template('auth/register.html')
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
